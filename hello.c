48d4d50d9343c9ee6bb4360c7ce818c3401bea53
Alexander Knight <72681685+alexander-knight@users.noreply.github.com>
2020-10-11 14:53:55 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*added this comment and learning syntax for c is strange. i guess i got spoiled by pyhons user friendliness
+the #include is what imports the module, and the module stdio.h is what containts the "printf: function. */
+
+int main(void)
+/* the line above is what starts the program.... somehow... look, im new to this, and the lecture hasnt explained how this but works, aight?*/
+{
+    printf("Hello, World! \n");
+}
